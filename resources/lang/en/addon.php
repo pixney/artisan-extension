daee694fc819ed212f1a679c9cb8baca662baf54
William <william@pixney.com>
2019-03-19 15:01:16 +0100

    first commit

---
+++ resources/lang/en/addon.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title'       => 'Artisan',
+    'name'        => 'Artisan Extension',
+    'description' => ''
+];
---
+++ tests/Feature/ArtisanExtensionTest.php
@@ -0,0 +1,10 @@
+<?php namespace Pixney\ArtisanExtension\Test\Feature;
+
+class ArtisanExtensionTest extends \TestCase
+{
+
+    public function testFeature()
+    {
+        $this->markTestSkipped('Not implemented.');
+    }
+}
